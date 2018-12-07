0fcc49e64730f1d8cdeac8838d1d64bef67fa2a4
Dan Finlay <dan@danfinlay.com>
2018-12-07 11:02:59 -0800

    Add log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Web3 = require('web3')
 
 const w = new Web3(web3.currentProvider)
 
+console.log('requesting latest block')
 w.eth.getBlock('latest')
 .then((block) => {
   console.log('block returned!', block)
