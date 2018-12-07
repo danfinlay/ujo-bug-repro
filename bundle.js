46f23cc0d04adf0415c7bb6b3e15028da78853d4
Dan Finlay <dan@danfinlay.com>
2018-12-07 11:03:06 -0800

    Add log

--- bundle.js
+++ bundle.js
@@ -24773,6 +24773,7 @@ const Web3 = require('web3')
 
 const w = new Web3(web3.currentProvider)
 
+console.log('requesting latest block')
 w.eth.getBlock('latest')
 .then((block) => {
   console.log('block returned!', block)
